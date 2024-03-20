f72187c044d8ff6faeb6f3c8a67d3a90be964ebc
Ravitejapatnala <128287646+Ravitejapatnala@users.noreply.github.com>
2024-03-20 11:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').toLowerCase();
+}
+
+touristSpots.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#bands').innerHTML =
+  touristSpots.map(band => `<li>${band}</li>`).join('');
